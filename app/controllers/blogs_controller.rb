b2595e3ae6f3db966e098352498314a10ee6f6c9
若杉志帆 <wakasugishiho@wakasugishihonoMacBook-Air.local>
2019-10-10 11:48:12 +0900

    [finish]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,7 +23,7 @@ class BlogsController < ApplicationController
   private
 
   def blog_params
-  	params.require(:blog).permit(:title, :body)
+  	params.require(:blog).permit(:title, :category, :body)
   end
 
 end
